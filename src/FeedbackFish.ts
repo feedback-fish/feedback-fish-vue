a5c902e191d7c22fced5429820d5686d0fbc501c
Michaël De Boey <info@michaeldeboey.be>
2020-09-28 09:57:22 +0200

    chore: run Prettier on full codebase

--- src/FeedbackFish.ts
+++ src/FeedbackFish.ts
@@ -2,20 +2,19 @@ export const FeedbackFish = {
   mounted() {
     // Inject script in $nextTick to make sure the entire app has mounted
     this.$nextTick(() => {
-      const ffScript = document.createElement("script");
+      const ffScript = document.createElement("script")
       ffScript.setAttribute(
         "src",
         `https://feedback.fish/ff.js?pid=${this.$props.projectId}`
-      );
-      ffScript.defer = true;
-      document.body.appendChild(ffScript);
-    });
+      )
+      ffScript.defer = true
+      document.body.appendChild(ffScript)
+    })
   },
   props: {
-    projectId: String
+    projectId: String,
   },
   render: function () {
-    return null;
-  }
-};
-
+    return null
+  },
+}
